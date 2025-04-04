3ca19383296aa48644135e2b341ae4bc03b2c4bc
Michał Nosiadek <nosiadek.michal@gmail.com>
2025-04-04 22:30:11 +0200

    add new home page layout

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,9 +2,22 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { LoginService } from '../login/login.service';
 
 @Component({
-    selector: 'app-home-page',
-    templateUrl: './home-page.component.html',
-    styleUrls: ['./home-page.component.css'],
-    standalone: false
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+  standalone: false,
 })
-export class HomePageComponent {}
+export class HomePageComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in');
+        }
+      });
+    });
+
+    const targets = document.querySelectorAll('.animate-on-scroll');
+    targets.forEach((t) => observer.observe(t));
+  }
+}
