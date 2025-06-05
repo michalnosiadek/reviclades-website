d54074b6eb94d333895105f24a3e1b73e7f0b5b3
Michał Nosiadek <nosiadek.michal@gmail.com>
2025-06-05 16:36:32 +0200

    optimizied characters page

--- src/app/characters-page/characters-page.component.ts
+++ src/app/characters-page/characters-page.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { Tooltip } from 'bootstrap';
+
 @Component({
   selector: 'app-characters-page',
   templateUrl: './characters-page.component.html',
@@ -8,12 +9,6 @@ import { Tooltip } from 'bootstrap';
 })
 export class CharactersPageComponent implements AfterViewInit {
   development: boolean = false;
-  ngAfterViewInit() {
-    const tooltipTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="tooltip"]'
-    );
-    tooltipTriggerList.forEach((el) => new Tooltip(el));
-  }
 
   selectedTabs: { [characterId: string]: number } = {
     fireBerserker: 0,
@@ -22,6 +17,38 @@ export class CharactersPageComponent implements AfterViewInit {
     batPriest: 0,
     cryptShambler: 0,
   };
+
+  ngAfterViewInit() {
+    // Tooltips
+    const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"]'
+    );
+    tooltipTriggerList.forEach((el) => new Tooltip(el));
+
+    // Lazy loading for videos
+    const videos = document.querySelectorAll('video');
+    const observer = new IntersectionObserver(
+      (entries, obs) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const video = entry.target as HTMLVideoElement;
+            const source = video.querySelector('source');
+            if (source && source.getAttribute('data-src')) {
+              source.setAttribute('src', source.getAttribute('data-src')!);
+              source.removeAttribute('data-src');
+              video.load(); // Triggers video loading
+              obs.unobserve(video); // Optional: stop observing once loaded
+            }
+          }
+        });
+      },
+      {
+        threshold: 0.25, // Load when 25% of video is in view
+      }
+    );
+
+    videos.forEach((video) => observer.observe(video));
+  }
 
   onTabChange(characterId: string, tabIndex: number): void {
     this.selectedTabs[characterId] = tabIndex;
